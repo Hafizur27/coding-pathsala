a5c21308843436e47aff146a70bc917b9516d77a
hafizur27 <mahmadhafiz33@gmail.com>
2023-04-01 17:46:45 +0600

    showing toast

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import Header from './components/Header/Header'
 import AllData from './components/AllData/AllData'
 import Question from './components/Qusetion/Question'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +16,7 @@ function App() {
      <Header></Header>
      <AllData></AllData>
      <Question></Question>
+     <ToastContainer></ToastContainer>
     </div>
   )
 }
--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { ToastContainer, toast } from 'react-toastify';
 
 const Data = (props) => {
     const {name, title, cover, profile, read_time, published_date} = props.data;
     const handelReadBtn = props.handelReadBtn;
     const handelBookMarkBtn = props.handelBookMarkBtn;
+    const showToast = () =>{
+        toast('Already bookmarked this book.')
+    }
     return (
         <>
-            <div className="card w-full bg-base-100 shadow-xl mb-6">
-                <figure><img src={cover} alt="Shoes" /></figure>
+            <div className="card w-full bg-base-100 shadow-xl mb-6 p-4">
+                <figure><img src={cover} alt="" /></figure>
                 <div className="card-body">
-                    <div className='flex justify-between align-middle'>
-                    <div className='flex gap-5'>
+                    <div className='md:flex md:justify-between md:align-middle'>
+                    <div className='md:flex md:gap-5'>
                         <img className='h-14 w-14 rounded-full' src={profile} alt="" />
                         <div>
                             <h4 className='font-bold'>{name}</h4>
-                            <p className='font-medium'>March {published_date}</p>
+                            <p className='font-medium'>Published : March {published_date}</p>
                         </div>
                     </div>
-                    <div className='flex gap-3'>
-                        <p>{read_time} min read</p>
-                        <p onClick={() => handelBookMarkBtn(props.data)}><FontAwesomeIcon icon={faBookmark} /></p>
+                    <div className='md:flex md:gap-3'>
+                        <p>Read-Time : {read_time} min</p>
+                        <p  onClick={() => {handelBookMarkBtn(props.data); showToast();}}><FontAwesomeIcon icon={faBookmark} /></p>
 
                     </div>
                     </div>
                     <div>
-                    <h2 className="card-title font-extrabold">{title}</h2>
+                    <h2 className="card-title font-extrabold my-4">{title}</h2>
                     <button onClick={()  => handelReadBtn(props.data)} className='btn  btn-link text-primary p-0'>Mark as read</button>
                     </div>
                 </div>
